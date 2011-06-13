4d9335d937ebfa4ba9b87cb9708908a04af3f898
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-06-13 12:13:20 +0100

    Initial gem skeleton created using Bundler

---
+++ lib/yahoofinance-symbolsuggest.rb
@@ -0,0 +1,7 @@
+require "yahoofinance-symbolsuggest/version"
+
+module Yahoofinance
+  module Symbolsuggest
+    # Your code goes here...
+  end
+end
---
+++ lib/yahoofinance-symbolsuggest/version.rb
@@ -0,0 +1,5 @@
+module Yahoofinance
+  module Symbolsuggest
+    VERSION = "0.0.1"
+  end
+end
